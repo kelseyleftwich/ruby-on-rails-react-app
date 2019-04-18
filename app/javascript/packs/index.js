3568d814a42811d3ec5c14396712ffa74ed6a9b5
Kelsey Leftwich <kelsey.leftwich@gmail.com>
2019-04-18 09:13:56 -0500

    Adds router to index

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -1,14 +1,13 @@
-// Run this example by adding <%= javascript_pack_tag 'hello_react' %> to the head of your layout file,
-// like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
-// of the page.
-
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRoute as Router, Route } from "react-router-dom";
 import App from "../components/App";
 
 document.addEventListener("DOMContentLoaded", () => {
   ReactDOM.render(
-    <App />,
+    <Router>
+      <Route path="/" component={App} />
+    </Router>,
     document.body.appendChild(document.createElement("div"))
   );
 });

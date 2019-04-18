897a859f05fca175f4a330b1b7869c9734c36b1d
Kelsey Leftwich <kelsey.leftwich@gmail.com>
2019-04-18 10:16:42 -0500

    Adds content to Home component

--- app/javascript/components/Home.js
+++ app/javascript/components/Home.js
@@ -6,7 +6,47 @@ class Home extends Component {
     this.state = {};
   }
   render() {
-    return <div>Home</div>;
+    return (
+      <>
+        <div className="row">
+          <div className="col p-3 m-3">
+            <h1 className="display-1">Zonkar</h1>
+            <h5 className="text-muted">Design Inclusion. Tech Driven.</h5>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-sm-6">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Let's move things forward</h5>
+                <p className="card-text">
+                  With supporting text below as a natural lead-in to additional
+                  content.
+                </p>
+                <a href="#" className="btn btn-primary">
+                  Learn more
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-6">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Become part of the story</h5>
+                <p className="card-text">
+                  With supporting text below as a natural lead-in to additional
+                  content.
+                </p>
+                <a href="#" className="btn btn-primary">
+                  Learn more
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    );
   }
 }
 

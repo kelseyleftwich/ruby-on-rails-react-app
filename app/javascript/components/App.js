ffd3d0d129f6f6f31c03611d6b266f645bf7de96
Kelsey Leftwich <kelsey.leftwich@gmail.com>
2019-04-18 09:51:37 -0500

    Adds navbar

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,12 +1,31 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, NavLink } from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
 
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <div className="container">
+
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <a className="navbar-brand" href="#">Ruby on Rails with React App</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item active">
+                <NavLink to="/" activeClassName="nav-link" className="nav-link">Home</NavLink>
+              </li>
+              <li class="nav-item">
+                <NavLink to="/about" activeClassName="nav-link active" className="nav-link">About</NavLink>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/about" component={About} />
